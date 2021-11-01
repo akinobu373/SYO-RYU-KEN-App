194f95e6b9bf7b9f641d1b6ecbe008ab6004a75c
akinobu <siguiente.football.373@gmail.com>
2021-11-01 13:56:39 +0900

    昇竜拳アプリが完成したよ！

--- SYO-RYU-KEN App/ViewController.swift
+++ SYO-RYU-KEN App/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageArrayAttak : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        while let attakImage = UIImage(named: "attak\(imageArrayAttak.count+1)") {
+            imageArrayAttak.append(attakImage)
+        }
+        
     }
-
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func tapImageButton(_ sender: Any) {
+        imageView.animationImages = imageArrayAttak
+        imageView.animationDuration = 3
+        imageView.animationRepeatCount = 1
+        imageView.startAnimating()
+    }
+    
 }
+
+
 
